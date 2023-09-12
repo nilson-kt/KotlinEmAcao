537037e7e60e2b65a8b34313daf2d4c1de98427d
Nilson <python.nilson@gmail.com>
2023-09-12 16:01:49 -0300

    adicao de exercicio

---
+++ 3. Operadores Aritmeticos/007 - Media Aritmetica/media_aritmetica_07.kt
@@ -0,0 +1,10 @@
+// Programa que lê duas notas de um aluno e exibe a média aritmética entre elas.
+
+fun main() {
+    print("Digite a primeira nota: ")
+    val nota1 = readln().toFloat()
+    print("Digite a segunda nota: ")
+    val nota2 = readln().toFloat()
+    val media = (nota1+nota2)/2
+    print("A média entre $nota1 e $nota2 é igual a $media")
+}
