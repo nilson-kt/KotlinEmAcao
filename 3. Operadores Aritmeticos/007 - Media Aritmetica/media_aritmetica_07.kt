45cb526d6ca04cecb29f075ce2d962cb213c6b7f
Nilson <python.nilson@gmail.com>
2023-09-16 19:43:31 -0300

    atualizacao de exercicio

--- 3. Operadores Aritmeticos/007 - Media Aritmetica/media_aritmetica_07.kt
+++ 3. Operadores Aritmeticos/007 - Media Aritmetica/media_aritmetica_07.kt
@@ -1,10 +1,15 @@
 // Programa que lê duas notas de um aluno e exibe a média aritmética entre elas.
 
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
+
 fun main() {
     print("Digite a primeira nota: ")
     val nota1 = readln().toFloat()
     print("Digite a segunda nota: ")
     val nota2 = readln().toFloat()
     val media = (nota1+nota2)/2
-    print("A média entre $nota1 e $nota2 é igual a $media")
+    val formato = DecimalFormat("0.00", DecimalFormatSymbols(Locale.US))
+    print("A média entre $nota1 e $nota2 é igual a ${formato.format(media)}")
 }
