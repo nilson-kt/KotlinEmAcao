4a4dea379f1ae89759c18b9a6e623069a2864aee
Nilson <python.nilson@gmail.com>
2023-10-01 18:52:42 -0300

    atualizacao exercicio

--- 3. Operadores Aritmeticos/010 - Conversor de Moedas/10_conversor_de_moedas.kt
+++ 3. Operadores Aritmeticos/010 - Conversor de Moedas/10_conversor_de_moedas.kt
@@ -1,6 +1,4 @@
 // Algoritmo que lê um valor em reais e exibe o mesmo valor convertido em dólar.
-import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
 import java.util.Locale
 
 fun main() {
@@ -8,8 +6,6 @@ fun main() {
     val real = readln().toDouble()
     println("Beleza! Agora mostrarei quantos dólares você pode comprar com esse valor!:")
     val dolar = real / 3.27
-    val formato = DecimalFormat("0.00", DecimalFormatSymbols(Locale.US))
-    val numeroFormatado = formato.format(dolar)
+    val numeroFormatado = String.format(Locale.US, "%.2f", dolar)
     print("U$${numeroFormatado}")
-
 }
