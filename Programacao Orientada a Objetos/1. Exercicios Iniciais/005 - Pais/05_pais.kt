0900bff1552c7de4ea6644e159260cefa0a91737
Nilson <python.nilson@gmail.com>
2023-12-04 12:23:37 -0300

    atualizacao de exercicio

--- Programacao Orientada a Objetos/1. Exercicios Iniciais/005 - Pais/05_pais.kt
+++ Programacao Orientada a Objetos/1. Exercicios Iniciais/005 - Pais/05_pais.kt
@@ -4,8 +4,8 @@ class Pais(iso: String, nome: String, dimensao: Double) {
     private var _iso: String
     private var _nome: String
     private var _dimensao: Double
-
     private var _fronteiras = mutableListOf<String>()
+
 
     init {
         _iso = iso
@@ -50,17 +50,35 @@ class Pais(iso: String, nome: String, dimensao: Double) {
     }
 
     fun verificarFronteira(pais: Pais): Boolean {
-        return (pais._nome in fronteiras)
+        return (pais._nome in _fronteiras)
+    }
+
+    fun verificarFronteirasEmComum(pais: Pais): MutableList<String> {
+        return pais._fronteiras.union(_fronteiras).toMutableList()
     }
 
 }
 
 
 fun main() {
-    val brasil = Pais("BRA", "Brasil", 8510417.771)
-    brasil.fronteiras = mutableListOf("Bolívia, Peru, Venezuela")
-    println(brasil.iso)
-    println(brasil.fronteiras)
-    println(brasil.nome)
-
+    val brasil = Pais("BRA", "Brasil", 8_510_417.771)
+    val venezuela = Pais("VEN", "Venezuela", 912_050.0)
+    val chile = Pais("CHL", "Chile", 756_945.0)
+    val equador = Pais("ECU", "Equador", 283_561.0)
+    val paraguai = Pais("PRY", "Paraguai", 406_752.0)
+    val listaPaises = mutableListOf(brasil, venezuela, chile, equador, paraguai)
+    println("-".repeat(42))
+    println(buildString {
+        append("ISO".padEnd(5))
+        append("Nome".padEnd(20))
+        append("Dimensão")
+    })
+    listaPaises.forEach {
+        println(buildString {
+            append(it.iso.padEnd(5))
+            append(it.nome.padEnd(20))
+            append(it.dimensao)
+        })
+    }
+    println("-".repeat(42))
 }
