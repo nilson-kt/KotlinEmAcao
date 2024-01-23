f11b1427b1f9da61383581e0a8917acfc9db1397
Nilson <python.nilson@gmail.com>
2024-01-23 19:00:48 -0300

    atualizacao de exercicio

--- Programacao Orientada a Objetos/1. Exercicios Iniciais/034 - Interface e Videos/interface_e_videos_34.kt
+++ Programacao Orientada a Objetos/1. Exercicios Iniciais/034 - Interface e Videos/interface_e_videos_34.kt
@@ -1,12 +1,20 @@
 fun main() {
 
-    val videos = arrayOfNulls<Video>(5)
+    val v = arrayOfNulls<Video>(5)
 
-    videos[0] = Video("Aula 1 de POO")
-    videos[1] = Video("Aula 12 de PHP")
-    videos[2] = Video("Aula 10 DE HTML5")
+    v[0] = Video("Aula 1 de POO")
+    v[1] = Video("Aula 12 de PHP")
+    v[2] = Video("Aula 10 DE HTML5")
 
-    println(videos[0].toString())
+    val g = arrayOfNulls<Gafanhoto>(2)
+
+
+    g[0] = Gafanhoto("Jubileu", 22, "M", "juba")
+    g[1] = Gafanhoto("Creuza", 1, "M", "creuzita")
+
+    g[0]
+
+    println(g[0].toString())
 
 }
 
@@ -26,42 +34,52 @@ class Video(var titulo: String): AcoesVideo {
     var reproduzindo = false
 
     override fun play() {
-        TODO("Not yet implemented")
+        reproduzindo = true
     }
 
     override fun pause() {
-        TODO("Not yet implemented")
+        reproduzindo = false
     }
 
     override fun like() {
-        TODO("Not yet implemented")
+        curtidas++
     }
 
     override fun toString(): String {
-        return """Video(titulo='$titulo', avaliacao=$avaliacao, views=$views, curtidas=$curtidas,
-             reproduzindo=$reproduzindo)"""
+        return "Video(titulo='$titulo', avaliacao=$avaliacao, views=$views, curtidas=$curtidas," +
+                " reproduzindo=$reproduzindo"
     }
 
 
 }
 
-open class Pessoa {
-    protected var nome = ""
-    protected var idade = 0
-    protected var sexo = ""
+abstract class Pessoa(protected var nome: String, protected var idade: Int, protected var sexo: String) {
     protected var experiencia = 0
 
     protected fun ganharExp() {
 
     }
+
+    override fun toString(): String {
+        return "Pessoa(nome='$nome', idade=$idade," +
+                " sexo='$sexo', experiencia=$experiencia)"
+    }
+
+
 }
 
-class Gafanhoto {
-    private val login = ""
-    private val totAssistido = ""
+class Gafanhoto(nome: String, idade: Int, sexo: String, private val login: String)
+    : Pessoa(nome, idade, sexo) {
+
+    private val totAssistido = 0
 
     fun viuMaisUm() {
 
     }
+
+    override fun toString(): String {
+        return "Gafanhoto(${super.toString()}, login='$login',  totAssistido=$totAssistido)"
+    }
+
 
 }
