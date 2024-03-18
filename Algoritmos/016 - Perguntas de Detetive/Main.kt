fcba4a343babfd3989a02059d6be54ba04f21b6c
sdcnpractice <sdcn888@gmail.com>
2024-03-18 12:40:07 -0300

    atualizacao de exercicio

--- Algoritmos/016 - Perguntas de Detetive/Main.kt
+++ Algoritmos/016 - Perguntas de Detetive/Main.kt
@@ -3,18 +3,18 @@ import kotlin.system.exitProcess
 fun main() {
     val vetor = arrayOf("Telefonou para a vítima?", "Esteve no local do crime?",  "Mora perto da vítima?",
         "Devia para a vítima?", "Já trabalhou com a vítima?")
+    var pontos = 0
     for (pergunta in vetor) {
-        print("$pergunta [1] Sim [2] Não")
-        val resposta = readln().toIntOrNull() ?: loop(pergunta)
+        do {
+            print("$pergunta ([1] Sim [2] Não [0] Encerrar programa):  ")
+            val resposta = readln().toIntOrNull()
+            when (resposta) {
+                null -> println("Digite um número inteiro 0, 1, 2.")
+                !in setOf(0, 1, 2) -> println("Opção inválida.")
+                0 -> exitProcess(0)
+                1 -> pontos++
+            }
+        } while (resposta !in setOf(0, 1, 2))
     }
 }
 
-fun loop(pergunta: String): Int {
-    do {
-        print("Resposta inválida. Digite 1 para Sim e 2 para Não ou digite 0 para passar para a próxima pergunta: ")
-        val resposta = readln().toInt()
-        if (resposta in setOf(1, 2)) return resposta
-    } while (resposta != 0)
-    println("Programa encerrado.")
-    exitProcess(0)
-}
