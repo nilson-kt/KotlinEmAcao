2b371fe735ab0493513ecb1df812a2c76c22148e
sdcnpractice <sdcn888@gmail.com>
2024-03-20 14:03:50 -0300

    atualizacao de exercicio

--- Algoritmos/016 - Perguntas de Detetive/Main.kt
+++ Algoritmos/016 - Perguntas de Detetive/Main.kt
@@ -1,5 +1,3 @@
-import kotlin.system.exitProcess
-
 fun main() {
         Detetive.entrada()
     }
@@ -9,11 +7,31 @@ object Detetive {
     val perguntas = arrayOf("Telefonou para a vítima?", "Esteve no local do crime?",  "Mora perto da vítima?",
         "Devia para a vítima?", "Já trabalhou com a vítima?")
 
+    val respostas = mutableListOf<Int>()
+
+    var pontos = 0
+
+    var classificacao = ""
+
     fun entrada() {
         for (pergunta in perguntas) {
             print("$pergunta ([1] Sim [2] Não [0] Encerrar programa):  ")
-            val resposta = Leitor.lerNumero()
+            respostas.add(Leitor.lerResposta())
         }
+    }
+
+    fun processamento() {
+        respostas.forEach { if (it == 1) pontos++ }
+        classificacao = when (pontos) {
+            2 -> "suspeito"
+            3, 4 -> "cúmplice"
+            5 -> "assassino"
+            else -> "inocente"
+        }
+    }
+
+    fun saida() {
+        println("Você é $classificacao")
     }
 }
 
@@ -21,20 +39,20 @@ object Leitor {
     private const val MAX_TENTATIVA = 5
     private var tentativa = 0
 
-    private fun lerEntrada(mensagem: String, validacao: (String) -> Boolean ) {
+    private fun lerEntrada(mensagem: String, validacao: (String) -> Boolean ): String {
         while (tentativa != MAX_TENTATIVA) {
             val entrada = readln()
-            if (!validacao(entrada)) print(mensagem)
+            if (!validacao(entrada)) print(mensagem) else return entrada
             tentativa++
         }
         throw MaximoDeTentativasAtingido()
     }
 
-    fun lerNumero() {
-        val numero = lerEntrada("Resposta inválida. Digite um número inteiro de 0, 1, 2: ") {
+    fun lerResposta(): Int {
+        return lerEntrada("Resposta inválida. Digite um número inteiro de 0, 1, 2: ") {
             val possivelNumero = it.toIntOrNull()
             possivelNumero != null && possivelNumero in setOf(0, 1, 2)
-        }
+        }.toInt()
     }
 }
 
