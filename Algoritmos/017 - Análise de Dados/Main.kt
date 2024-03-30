876596f989ed24f48d2cf24d96c55607adb18214
sdcnpractice <sdcn888@gmail.com>
2024-03-30 17:27:41 -0300

    update exercicio

--- Algoritmos/017 - Análise de Dados/Main.kt
+++ Algoritmos/017 - Análise de Dados/Main.kt
@@ -1,10 +1,25 @@
 fun main() {
-
+    Sistema.lerEntrada()
 }
 
 object Sistema {
+    private val lista = mutableListOf<Int>()
 
     fun lerEntrada() {
         print("Digite um número inteiro: ")
+        while (true) {
+            when (val entrada = readln().toIntOrNull())  {
+                null -> {
+                    print("Entrada inválida. Digite um número inteiro: ")
+                    continue
+                }
+                -1 -> break
+                else -> lista.add(entrada)
+            }
+        }
+    }
+
+    fun processar() {
+
     }
 }
