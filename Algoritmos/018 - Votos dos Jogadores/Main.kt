44078a2f585215531654274f291bd20c60f97055
sdcnpractice <sdcn888@gmail.com>
2024-05-09 13:44:06 -0300

    update exercicio

--- Algoritmos/018 - Votos dos Jogadores/Main.kt
+++ Algoritmos/018 - Votos dos Jogadores/Main.kt
@@ -7,24 +7,25 @@ data class Jogador(val nome: String, var voto: Int = 0, var porcentagem: String 
 }
 
 object Sistema {
-    var totalVotos = 0
-    val vetor = List(23) {Jogador("Jogador ${it+1}", 0, "")}
-    lateinit var jogadoresVotados: List<Jogador>
-    lateinit var melhorJogador: Jogador
+    private var totalVotos = 0
+    private val vetor = List(23) {Jogador("Jogador ${it+1}", 0, "")}
+    private lateinit var jogadoresVotados: List<Jogador>
+    private lateinit var melhorJogador: Jogador
 
-    fun entrada() {
+    private fun entrada() {
         while (true) {
             print("Digite um número entre 1 e 23 (0 para encerrar): ")
             when (val numero = readln().toIntOrNull()) {
                 null -> println("Número inválido. Digite um número inteiro.")
-                !in (0..23) -> println("Número inválido. Digite um número entre 1 e 23.")
+                !in (0..23) -> println("Número inválido. Digite um número entre 1 e 23 para votar ou digite" +
+                        " 0 para encerrar.")
                 0 -> break
                 else -> vetor[numero-1].voto++
             }
         }
     }
 
-    fun processamento() {
+    private fun processamento() {
         totalVotos = vetor.sumOf { it.voto }
         for (jogador in vetor) {
             jogador.porcentagem = "${((jogador.voto.toFloat() / totalVotos) * 100).toInt()}%"
@@ -34,17 +35,20 @@ object Sistema {
 
     }
 
-    fun output() {
+    private fun output() {
+        println("-".repeat(40))
+        println("RESULTADO DA VOTAÇÃO")
+        println("-".repeat(40))
         println("Foram computados $totalVotos votos.")
         jogadoresVotados.forEach {
             println(buildString {
                 append(it.nome.padEnd(14))
-                append(it.voto.toString().padEnd(4))
+                append(it.voto.toString().padEnd(6))
                 append(it.porcentagem)
             })
         }
         melhorJogador.run {
-            println("O melhor jogador foi o ${this.nome}, com ${this.voto} votos, correspondnedo a ${this.porcentagem}" +
+            println("O melhor jogador foi o ${this.nome}, com ${this.voto} votos, correspondendo a ${this.porcentagem}" +
                     " do total de votos.")
         }
     }
